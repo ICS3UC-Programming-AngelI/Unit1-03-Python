061ddc6dc22bca0b394a4e3a6b1e77b7b7a524fc
Angel Irangabiye <angel.irangabiyr@stu.ocsb.ca>
2025-02-19 16:34:02 +0000

    Added hello_world_style.py

---
+++ hello_world_style.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# Created By: Angel
+# Date: Feb 13,2025
+# This program displays Hello,World! with proper style.
+
+
+def main():
+    print("Helo,World! with proper style")
+    print("I am testing my function here")
+
+
+if __name__ == "__main__":
+    main()
